ccd24bc82365a5f28aa260fac04250c93ddda47f
ayon121 <borshonsaha1234@gmail.com>
2023-12-21 18:05:51 +0600

    why us section  updated

--- src/Components/Pages/Home/WhyTaskPulse/WhyTaskPulse.jsx
+++ src/Components/Pages/Home/WhyTaskPulse/WhyTaskPulse.jsx
@@ -4,15 +4,16 @@ import whypic from '../../../../assets/images/whyus.jpg'
 
 const WhyTaskPulse = () => {
     return (
-        <div className="hero min-h-screen bg-base-200 mt-4 max-w-7xl mx-auto rounded-l-3xl mb-5 px-4 font-poppins">
+        <div className="hero min-h-screen bg-base-200 mt-4 max-w-7xl mx-auto rounded-3xl md:rounded-l-3xl mb-5 px-4 font-poppins">
             <div className="hero-content flex-col-reverse lg:flex-row-reverse">
-                <img src={whypic} className="max-w-sm w-4/5  rounded-lg shadow-2xl" />
-                <div>
-                    <h1 className="flex items-center gap-2 text-2xl md:text-4xl lg:text-5xl font-bold">Why 
+                <img src={whypic} className="max-w-sm w-full hidden md:flex  rounded-lg shadow-2xl" />
+                <div >
+                    <h1 className="flex justify-center md:justify-start items-center gap-2 text-4xl lg:text-5xl font-bold">Why 
                     <span>
-                    <a className=" text-xl md:text-2xl lg:text-4xl text-main flex items-center"><span className="text-2xl md:text-4xl lg:text-5xl">Task</span><MdOutlineTaskAlt />Pulse</a>
+                    <a className="text-2xl lg:text-4xl text-main flex items-center"><span className="text-4xl lg:text-5xl">Task</span><MdOutlineTaskAlt />Pulse</a>
                     </span>
                     </h1>
+                    <img src={whypic} className="max-w-sm w-full mx-auto mt-3 flex  md:hidden  rounded-lg shadow-2xl" />
                     <p className="py-6">Choose our task manager for a seamless blend of efficiency and user-friendly design. With intuitive features, robust organization tools, and a commitment to enhancing your productivity, we provide the ideal platform for individuals seeking a reliable companion on their journey towards unparalleled task management success. </p>
                     <h1>1.Free To Use</h1>
                     <h1>2.Smart Organization</h1>
