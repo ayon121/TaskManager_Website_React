fd44f57c4c86acaaac421bf455a813919781c4f0
ayon121 <borshonsaha1234@gmail.com>
2023-12-22 15:58:11 +0600

    animation added

--- src/Components/Pages/Home/Banner/Banner.jsx
+++ src/Components/Pages/Home/Banner/Banner.jsx
@@ -1,7 +1,12 @@
 import { Link } from 'react-router-dom';
 import banner from '../../../../assets/images/banner.jpg'
-
+import Aos from "aos";
+import "aos/dist/aos.css"
+import { useEffect } from "react";
 const Banner = () => {
+    useEffect(()=> {
+        Aos.init({duration : 400})
+    },[])
     return (
         <div className="hero h-auto md:rounded-br-full max-w-full mb-5" style={{ backgroundImage: `url(${banner})` }}>
             <div className="hero-overlay opacity-85 md:rounded-br-full max-w-full"></div>
@@ -10,7 +15,7 @@ const Banner = () => {
                     <h1 className="mb-2 md:mb-3 lg:mb-5 text-2xl md:text-4xl lg:text-6xl font-bold text-sub_color space-y-4">Free</h1>
                     <h1 className="mb-2 md:mb-3 lg:mb-5 text-2xl md:text-4xl lg:text-6xl font-bold text-sub_color space-y-4">Online Task Manager</h1>
                     <p className="mb-5 w-4/5">Empower your productivity journey with our sleek and intuitive task manager — where efficiency meets elegance, and every to-do becomes a step closer to success. Stay organized, stay focused, and unlock the full potential of your day with our task manager designed to elevate your productivity game</p>
-                    <Link to="/login"><button className="btn  btn-sm md:btn-md lg:btn-lg bg-sub_color text-main">Lets Explore</button></Link>
+                    <Link to="/login"><button className="btn  btn-sm md:btn-md lg:btn-lg bg-sub_color text-main"  data-aos="fade-up-right">Lets Explore</button></Link>
                 </div>
             </div>
         </div>
--- src/Components/Pages/Home/WhyTaskPulse/WhyTaskPulse.jsx
+++ src/Components/Pages/Home/WhyTaskPulse/WhyTaskPulse.jsx
@@ -3,18 +3,25 @@ import { MdOutlineTaskAlt } from "react-icons/md";
 import whypic from '../../../../assets/images/whyus.jpg'
 import { Link } from "react-router-dom";
 
+import Aos from "aos";
+import "aos/dist/aos.css"
+import { useEffect } from "react";
+
 const WhyTaskPulse = () => {
+    useEffect(()=> {
+        Aos.init({duration : 700})
+    },[])
     return (
         <div className="hero min-h-screen bg-base-200 mt-4 max-w-7xl mx-auto rounded-3xl md:rounded-l-3xl mb-5 px-4 font-poppins">
             <div className="hero-content flex-col-reverse lg:flex-row-reverse">
-                <img src={whypic} className="max-w-sm w-full hidden md:flex  rounded-lg shadow-2xl" />
+                <img src={whypic} data-aos="fade-down" className="max-w-sm w-full hidden md:flex  rounded-lg shadow-2xl" />
                 <div >
-                    <h1 className="flex justify-center md:justify-start items-center gap-2 text-4xl lg:text-5xl font-bold">Why 
+                    <h1 className="flex justify-center md:justify-start items-center gap-2 text-4xl lg:text-5xl font-bold" data-aos="fade-right">Why 
                     <span>
                     <a className="text-2xl lg:text-4xl text-main flex items-center"><span className="text-4xl lg:text-5xl">Task</span><MdOutlineTaskAlt />Pulse</a>
                     </span>
                     </h1>
-                    <img src={whypic} className="max-w-sm w-full mx-auto mt-3 flex  md:hidden  rounded-lg shadow-2xl" />
+                    <img src={whypic} data-aos="fade-down" className="max-w-sm w-full mx-auto mt-3 flex  md:hidden  rounded-lg shadow-2xl" />
                     <p className="py-6">Choose our task manager for a seamless blend of efficiency and user-friendly design. With intuitive features, robust organization tools, and a commitment to enhancing your productivity, we provide the ideal platform for individuals seeking a reliable companion on their journey towards unparalleled task management success. </p>
                     <h1>1.Free To Use</h1>
                     <h1>2.Smart Organization</h1>
